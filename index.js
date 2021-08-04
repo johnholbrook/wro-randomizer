eab32a6a83243478ddba6393568c53bf3e18c882
John Holbrook <john@johnholbrook.us>
2021-08-04 16:36:58 -0400

    randomize junior field

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // global to keep track of the board to be randomized (elem, junior, or senior)
-var board = "elem";
+var board;
 
 // returns a random integer from min to max (inclusive)
 function rand_int(min, max){
@@ -41,6 +41,137 @@ function randomize_elem(){
         image.querySelector("#sun-top rect").style.fill = "none";
         image.querySelector("#sun-bottom rect").style.fill = "yellow";
     }
+}
+
+// junior: generate a random ordering of waiting cars in the lane
+function lane_seq(){
+    let values = ["red", "red", "blue", "blue", "green", "green"];
+    let result = [];
+    [1,2,3,4,5,6].forEach(_i => {
+        let tmp = rand_int(0, values.length-1);
+        result.push(values[tmp]);
+        values.splice(tmp,1);
+    });
+    return result;
+}
+
+// junior: generate locations for the two pillars
+function pillar_locs(){
+    let barriers = [];
+    let empty = [];
+    [1,2,3].forEach(i => {
+        let tmp = rand_int(0,1);
+        barriers.push(`${i}${tmp==0?"a":"b"}`);
+        empty.push(`${i}${tmp==0?"b":"a"}`);
+    });
+    return {"populated":barriers, "empty":empty};
+}
+
+// removes from a1 any items that are also in a2
+function array_sub(a1, a2){
+    let result = a1;
+    a2.forEach(elem => {
+        if (result.includes(elem)){
+            result.splice(result.indexOf(elem), 1);
+        }
+    });
+    return result;
+}
+
+// junior: generate locations for the two barriers
+function barrier_locs(){
+    let available_spaces = ["r1", "r2", "r3", "r4", "g1", "g2", "g3", "g4", "b1", "b2", "b3", "b4"];
+
+    // get the space names with parked cars and remove them from the list – they can't have barriers in front of them
+    let occupied_spaces = [
+        document.querySelector("#red-parked-car").value,
+        document.querySelector("#blue-parked-car").value,
+        document.querySelector("#green-parked-car").value
+    ];
+    available_spaces = array_sub(available_spaces, occupied_spaces);
+
+    // console.log(available_spaces);
+
+    // choose a location for the first barrier (any available space)
+    let loc1_id = rand_int(0, available_spaces.length-1);
+    let loc1 = available_spaces[loc1_id];
+    available_spaces.splice(loc1_id, 1);
+
+    // now we can eliminate some additional spaces - both barriers can't be in the same row or in front of the same color space
+    switch (loc1[0]){ // same color 
+        case "r":
+            available_spaces = array_sub(available_spaces, ["r1", "r2", "r3", "r4"]);
+        break;
+        case "g":
+            available_spaces = array_sub(available_spaces, ["g1", "g2", "g3", "g4"]);
+        break;
+        case "b":
+            available_spaces = array_sub(available_spaces, ["b1", "b2", "b3", "b4"]);
+        break;
+    }
+
+    switch (loc1){ // same row
+        case "r1":
+        case "r2":
+        case "g1":
+        case "b1":
+            available_spaces = array_sub(available_spaces, ["r1", "r2", "g1", "b1"]);
+        break;
+        case "g2":
+        case "r3":
+        case "g3":
+        case "b2":
+            available_spaces = array_sub(available_spaces, ["g2", "r3", "g3", "b2"]);
+        break;
+        case "b3":
+        case "b4":
+        case "r4":
+        case "g4":
+            available_spaces = array_sub(available_spaces, ["b3", "b4", "r4", "g4"]);
+        break;
+    }
+
+    // console.log(available_spaces);
+    // now available_spaces contains only the spaces that the second barrier could legally be in
+    let loc2 = available_spaces[rand_int(0, available_spaces.length-1)];
+
+    return [loc1, loc2];
+}
+
+// junior: clear all the barriers
+function clear_all_barriers(){
+    let image = document.querySelector("#field-image").contentDocument;
+    const locs = ["r1", "r2", "r3", "r4", "g1", "g2", "g3", "g4", "b1", "b2", "b3", "b4"];
+    locs.forEach(loc => {
+        image.querySelector(`#barrier-${loc} rect`).setAttribute("fill", "none");
+    });
+}
+
+// randomize the junior board
+function randomize_junior(){
+    let image = document.querySelector("#field-image").contentDocument;
+
+    // randomize the waiting cars in the lane
+    let lane_order = lane_seq();
+    lane_order.forEach((color, i) => {
+        image.querySelector(`#lane-${i+1} rect`).style.fill = color;
+    });
+
+    // randomize the pillars
+    let pillars = pillar_locs();
+    pillars.populated.forEach(id => {
+        image.querySelector(`#pillar-${id} rect`).style.fill = "white";
+    });
+    pillars.empty.forEach(id => {
+        image.querySelector(`#pillar-${id} rect`).style.fill = "none";
+    });
+
+    // randomize the barriers
+    clear_all_barriers();
+    let barriers = barrier_locs();
+    barriers.forEach(loc => {
+        image.querySelector(`#barrier-${loc} rect`).setAttribute("fill", "url('#barrier')");
+    });
 }
 
 // senior: generate a random ordering for the energy identifiers
@@ -116,6 +247,74 @@ function init_elem(){
     };
 }
 
+// junior: update the fills of the parking areas
+function update_parking(){
+    let image = document.querySelector("#field-image").contentDocument;
+    // clear all existing fills
+    const names = ["g1", "g2", "g3", "g4", "b1", "b2", "b3", "b4"];
+    names.forEach(name => {
+        image.querySelector(`#parking-${name} rect`).setAttribute("fill", "none");
+    });
+
+    // get values from the dropdowns & fill as appropriate
+    let occupied_spaces = [
+        {color: "red-parked", space: document.querySelector("#red-parked-car").value},
+        {color: "blue-parked", space: document.querySelector("#blue-parked-car").value},
+        {color: "green-parked", space: document.querySelector("#green-parked-car").value},
+    ];
+    occupied_spaces.forEach(car => {
+        image.querySelector(`#parking-${car.space} rect`).setAttribute("fill", `url('#${car.color}')`);
+    });
+}
+
+// initialize the page for the junior layout
+function init_jr(){
+    // write to the global variable
+    board = "junior";
+
+    // set the blank SVG
+    document.querySelector("#field-image").data = "junior_blank.svg";
+
+    // populate the options area, including callbacks
+    document.querySelector("#board-options").innerHTML = `<div class="input-group mb-3">
+            <span class="input-group-text">Red Parked Car: </span>
+            <select class="form-select" id="red-parked-car">
+                <option selected>Choose...</option>
+                <option value="g1">Top row, green</option>
+                <option value="b1">Top row, blue</option>
+                <option value="g2">Middle row, left green</option>
+                <option value="g3">Middle row, right green</option>
+                <option value="b2">Middle row, blue</option>
+                <option value="b3">Bottom row, left blue</option>
+                <option value="b4">Bottom row, right blue</option>
+                <option value="g4">Bottom row, green</option>
+            </select>
+        </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text">Blue Parked Car: </span>
+            <select class="form-select" id="blue-parked-car">
+                <option selected>Choose...</option>
+                <option value="b1">Top row, blue</option>
+                <option value="b2">Middle row, blue</option>
+                <option value="b3">Bottom row, left blue</option>
+                <option value="b4">Bottom row, right blue</option>
+            </select>
+        </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text">Green Parked Car: </span>
+            <select class="form-select" id="green-parked-car">
+                <option selected>Choose...</option>
+                <option value="g1">Top row, green</option>
+                <option value="g2">Middle row, left green</option>
+                <option value="g3">Middle row, right green</option>
+                <option value="g4">Bottom row, green</option>
+            </select>
+        </div>`
+    document.querySelector("#red-parked-car").onchange = update_parking;
+    document.querySelector("#green-parked-car").onchange = update_parking;
+    document.querySelector("#blue-parked-car").onchange = update_parking;
+}
+
 // initialiize the page for the senior layout
 function init_sr(){
     // write to the global variable
@@ -134,8 +333,7 @@ document.addEventListener("DOMContentLoaded", () => {
         init_elem();
     };
     document.querySelector("#field-jr").onclick = () => {
-        board = "junior";
-        document.querySelector("#field-image").data = "junior_blank.svg";
+        init_jr();
     };
     document.querySelector("#field-sr").onclick = () => {
         init_sr();
@@ -143,8 +341,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // callback to randomize the field
     document.querySelector("#randomize-btn").onclick = () => {
-        if (board = "elem"){
+        if (board == "elem"){
             randomize_elem();
+        }
+        else if (board == "junior"){
+            randomize_junior();
         }
         else if (board == "senior"){
             randomize_senior();
